ed7da3479ffbdbed318562d2287d621278457282
sana fathima <136157669+sanafathima7@users.noreply.github.com>
2024-03-08 00:21:50 +0530

    working

--- lib/presentation/login/tab_login.dart
+++ lib/presentation/login/tab_login.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: avoid_print
 
-import 'package:englishcoach/application/provider/user_provider_password_update.dart';
-import 'package:englishcoach/presentation/login/widget/button/FormField.dart';
+import 'package:englishcoach/presentation/login/widget/button/custom_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 class TabViewLogin extends StatefulWidget {
   const TabViewLogin({super.key});
@@ -21,15 +19,11 @@ class _TabViewLoginState extends State<TabViewLogin> {
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 184, 205, 221),
         body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: SingleChildScrollView(
             child: Column(
               children: [
-                const SizedBox(
-                  height: 120,
-                ),
+                const SizedBox(height: 120),
                 Text(
                   "englishcoach",
                   style: GoogleFonts.poppins(
@@ -44,39 +38,13 @@ class _TabViewLoginState extends State<TabViewLogin> {
                       color: Colors.black,
                       fontWeight: FontWeight.w400),
                 ),
-                const SizedBox(
-                  height: 60,
-                ),
-                const TextFormFieldSection(),
+                const SizedBox(height: 60),
+                const CustomPovider(), // Ensure this is directly under here
               ],
             ),
           ),
         ),
       ),
-    );
-  }
-}
-
-class TextFormFieldSection extends StatefulWidget {
-  const TextFormFieldSection({super.key});
-
-  @override
-  State<TextFormFieldSection> createState() => _TextFormFieldSectionState();
-}
-
-TextEditingController nameController = TextEditingController();
-TextEditingController passwordController = TextEditingController();
-final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-String errorMessage = '';
-
-class _TextFormFieldSectionState extends State<TextFormFieldSection> {
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<UserproviderPassword>(
-      create: (context) => UserproviderPassword(),
-      builder: (context, child) {
-        return LoginForm();
-      },
     );
   }
 }
